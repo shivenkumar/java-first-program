c59c61ff9e0e8a89bfda675d10100b8e50367752
shivenkumar <shiven.kumar26@gmail.com>
2022-06-02 20:35:49 -0400

    Handling error and bad inputs

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -46,12 +46,12 @@ public class SavingsCalculator {
 
         float[] credits = new float[creditsAsString.length];
         for (int i=0; i< creditsAsString.length; i++) {
-            credits[i] = Float.parseFloat(creditsAsString[i]);
+            credits[i] = Utilities.getFloatValue(creditsAsString[i]);
         }
 
         float[] debits = new float[debitsAsString.length];
         for (int i=0; i< debitsAsString.length; i++) {
-            debits[i] = Float.parseFloat(debitsAsString[i]);
+            debits[i] = Utilities.getFloatValue(debitsAsString[i]);
         }
 
         SavingsCalculator savingsCalculator = new SavingsCalculator(credits, debits);
